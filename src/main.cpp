986389215ee6b0c2244a8a125be0a723a0bcb463
Makar <anl-pr@yandex.ru>
2022-03-13 20:18:14 +0300

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <string>
 #include "fun.h"
 
-int main() {
+ 
+int main()
+{ 
+    const char * test = "  av     a1v    2 abcd   ";
+    const char* test1 = "Yyyyy yy YyYyYyYyy Yyyy YyyY ";
+    const char* test2 = " six monkeys";
+    unsigned int rez = faStr1(test);
+    unsigned int rez1 = faStr2(test1);
+    unsigned int rez2 = faStr3(test2);
+    std::cout << rez <<rez1 << rez2 ;
     return 0;
+}
 }
