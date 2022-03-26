17e068c45bc2e26ce16b94dda0d383bba0743657
Makar <anl-pr@yandex.ru>
2022-03-26 14:02:26 +0300

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 const char * test = "  av     a1v    2 abcd   ";
 const char* test1 = "Yyyyy yy YyYyYyYyy Yyyy YyyY ";
 const char* test2 = " six monkeys";
-unsigned int rez = faStr1(test);
-unsigned int rez1 = faStr2(test1);
-unsigned int rez2 = faStr3(test2);
+int rez = faStr1(test);
+int rez1 = faStr2(test1);
+int rez2 = faStr3(test2);
 }
