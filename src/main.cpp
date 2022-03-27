9aa3e0b49a1aba183f84e63740196f3f28aff35c
Makar <anl-pr@yandex.ru>
2022-03-27 08:50:23 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
 // Copyright 2022 UNN-IASR
-#include <iostream>
 #include "fun.h"
 
 int main() {
-const char * test = "  av     a1v    2 abcd   ";
-const char* test1 = "Yyyyy yy YyYyYyYyy Yyyy YyyY ";
-const char* test2 = " six monkeys";
-int rez = faStr1(test);
-int rez1 = faStr2(test1);
-int rez2 = faStr3(test2);
 return 0;
 }
